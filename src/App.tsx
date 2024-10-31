a59e6d75ea901a50688ebda9b0e1dac04dee3f07
Seoyeon Kim <kseoy0607@gmail.com>
2024-10-31 15:24:22 +0900

    feat: NavBar 컴포넌트 구현 - NavBar에 들어갈 페이지 생성 - 각 페이지 라우터 설정 - NavBar 컴포넌트 구현 Resolves: #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Router from './routes/Router';
 
 const App = () => {
-  return <Router></Router>;
+  return (
+    <>
+      <Router></Router>
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/Bbu.tsx
@@ -0,0 +1,4 @@
+const Bbu = () => {
+  return <h1 className="text-2xl font-bold">쀼</h1>;
+};
+export default Bbu;
