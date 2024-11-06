62993215fb3406f77ce66ebbd99dcb8b21405d95
Seoyeon Kim <kseoy0607@gmail.com>
2024-11-06 21:58:24 +0900

    fix: main.tsx에서 나는 eslint 오류 수정 파일이 React 컴포넌트만을 export 해야 한다는 규칙 위반 네비바 조건 함수는 App.tsx 로 옮김

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { useLocation } from 'react-router-dom';
 import Router from './routes/Router';
+import NavBar from './components/NavBar';
 
 const App = () => {
+  const location = useLocation();
+  const hideNavPaths = [
+    '/select-login',
+    '/consultant-chatbot',
+    '/login',
+    '/signup',
+    '/notification',
+    '/mypage',
+    '/password',
+    '/buy',
+    '/myheart',
+    '/mypage',
+    '/counselor-chatbot'
+  ]; // 네비바 숨길 경로 추가
+
   return (
-    <div className="app-container">
-      <Router></Router>
-    </div>
+    <>
+      {!hideNavPaths.includes(location.pathname) && <NavBar />}
+
+      <div className="app-container">
+        <Router></Router>
+      </div>
+    </>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,41 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
-import { BrowserRouter, useLocation } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
-import NavBar from './components/NavBar.tsx';
-
-const Root = () => {
-  const location = useLocation();
-  const hideNavPaths = [
-    '/select-login',
-    '/consultant-chatbot',
-    '/login',
-    '/signup',
-    '/notification',
-    '/mypage',
-    '/password',
-    '/buy',
-    '/myheart',
-    '/mypage',
-    '/counselor-chatbot'
-  ]; // 네비바 숨길 경로 추가
-
-  return (
-    <>
-      {!hideNavPaths.includes(location.pathname) && <NavBar />}
-
-      <App />
-    </>
-  );
-};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Root />
+        <App />
       </BrowserRouter>
     </Provider>
   </StrictMode>
