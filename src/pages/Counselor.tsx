d3a7ebfec4041a9ff32a65c5185043fba42086a5
gimeus <gimeus@naver.com>
2024-11-07 00:57:55 +0900

    feat: DropdownCategory 컴포넌트 구현

--- src/pages/Counselor.tsx
+++ src/pages/Counselor.tsx
@@ -1,10 +1,24 @@
+import React from 'react';
 import Header from '@/components/Header';
+import DropdownCategory from '@/components/DropdownCategory';
 
-const Counselor = () => {
+const Counselor: React.FC = () => {
+  const dropdownOptions = ['인기순', '만족도순'];
+
+  const handleCategorySelect = (selectedOption: string) => {
+    console.log('Selected category:', selectedOption);
+  };
+
   return (
     <>
       <Header title="상담" showDropdownIcon={true} />
-      <main className="content"></main>
+      <main className="content">
+        <DropdownCategory
+          label="맞춤 추천순"
+          options={dropdownOptions}
+          onSelect={handleCategorySelect}
+        />
+      </main>
     </>
   );
 };
