ed5f2dca246dd04648bb28c13b692b5856a26524
Seoyeon Kim <kseoy0607@gmail.com>
2024-11-06 14:48:51 +0900

    fix: 헤더 최대 너비 수정 425px -> 450px

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ const Header = ({
   title,
   showDropdownIcon = true,
   showSearchIcon = true,
-  showNotificationIcon = true,
+  showNotificationIcon = true
 }: HeaderProps) => {
   const navigate = useNavigate();
   const [isDropdownOpen, setDropdownOpen] = useState(false);
@@ -65,7 +65,7 @@ const Header = ({
 
 const HeaderContainer = styled.header`
   width: 100%;
-  max-width: 425px;
+  max-width: 450px;
   height: 64px;
   display: flex;
   align-items: center;
