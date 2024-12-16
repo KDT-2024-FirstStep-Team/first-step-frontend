4bcbe264245964dc02c77d95608611cf55a70a08
Seoyeon Kim <kseoy0607@gmail.com>
2024-12-16 16:19:37 +0900

    feat: myheart 페이지 마이그레이

--- src/app/myheart/page.tsx
+++ src/app/myheart/page.tsx
@@ -1,15 +1,19 @@
-import BackHeader from '@/components/BackHeader';
-import HeartTab from '@/components/HeartTab';
-import NotificationBanner from '@/components/NotificationBanner';
-import { useState } from 'react';
+'use client';
 
-const MyHeart = () => {
+import { useState } from 'react';
+import BackHeader from '../components/BackHeader';
+import CardBanner from '../components/CardBanner';
+import HeartTab from '../components/HeartTab';
+import NotificationBanner from '../components/NotificationBanner';
+
+export default function MyHeart() {
   const [activeTab, setActiveTab] = useState(0);
 
   return (
     <>
       <BackHeader title="내 하트" />
       <main className="content">
+        <CardBanner numberOfBbu={20} />
         <NotificationBanner
           src="/icons/notification-heart.svg"
           alt="Heart"
@@ -20,6 +24,4 @@ const MyHeart = () => {
       </main>
     </>
   );
-};
-
-export default MyHeart;
+}
